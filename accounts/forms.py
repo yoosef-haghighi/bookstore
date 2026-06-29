4530d4f0fe32444c3390ca283721b69fabb98f8b
yoosef-haghighi <yoosefhaghighi92@gmail.com>
2026-06-29 08:43:11 +0330

    completion custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,11 +5,14 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'age', 'email', )
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'age', 'email', )
+
+
+
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class SignUpView(generic.CreateView):
+    form_class = CustomUserCreationForm
+    template_name = 'registration/signup.html'
+    success_url = reverse_lazy('login')
+
