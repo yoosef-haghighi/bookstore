09ab327298ea72e3b9c0a89f8d1e8e52ad2ceb8a
yoosef-haghighi <yoosefhaghighi92@gmail.com>
2026-07-09 08:41:00 +0330

    add BookUpdateView

--- books/views.py
+++ books/views.py
@@ -20,4 +20,11 @@ class BookCreateView(generic.CreateView):
     template_name = 'books/book_create.html'
 
 
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
+
+
+
 
